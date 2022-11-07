c0b7e1f19c78ebfb5bd5da54ee0ddff1f7ea6bcc
pnumbers <pnumbers210@gmail.com>
2022-11-07 13:37:51 +0100

    Added decimal control to util

--- beam.js
+++ beam.js
@@ -1,28 +1,30 @@
 const lengthEntry = document.getElementById("length");
 const loadEntry = document.getElementById("linear-load");
-const ultMomentEntry = document.getElementById("ult-moment");
+const ultMomentEl = document.getElementById("ult-moment");
 const momentCapcityEntry = document.getElementById("moment-capacity");
 const calcButton = document.getElementById("calc-button");
 const momentUtilElement = document.getElementById("moment-util");
+
 
 function calculateMoment() {
     const length = lengthEntry.value;
     const linearLoad = loadEntry.value;
     const moment = linearLoad * (length ** 2) / 8;
-    ultMomentEntry.value = moment;
+    ultMomentEl.innerHTML = moment;
     checkCapcity();
 }
 
+function checkCapcity() {
+    calcMomentUtilization();
+}
+
 function calcMomentUtilization() {
-    const ultMoment = ultMomentEntry.value;
+    const ultMoment = parseFloat(ultMomentEl.innerHTML);
     const momentCapcity = momentCapcityEntry.value;
     let util = ultMoment / momentCapcity;
-    momentUtilElement.innerHTML = util
+    momentUtilElement.innerHTML = util.toFixed(2);
 
 }
 
 calcButton.addEventListener("click", calculateMoment);
 
-function checkCapcity() {
-    calcMomentUtilization();
-}
