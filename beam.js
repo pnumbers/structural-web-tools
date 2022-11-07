6ca1a2bf2c697f5c4d6ea79adb872065358a38e1
pnumbers <pnumbers210@gmail.com>
2022-11-07 12:41:49 +0100

    Added moment utilization check function

--- beam.js
+++ beam.js
@@ -1,14 +1,28 @@
 const lengthEntry = document.getElementById("length");
 const loadEntry = document.getElementById("linear-load");
 const ultMomentEntry = document.getElementById("ult-moment");
+const momentCapcityEntry = document.getElementById("moment-capacity");
 const calcButton = document.getElementById("calc-button");
+const momentUtilElement = document.getElementById("moment-util");
 
 function calculateMoment() {
     const length = lengthEntry.value;
     const linearLoad = loadEntry.value;
     const moment = linearLoad * (length ** 2) / 8;
     ultMomentEntry.value = moment;
+    checkCapcity();
+}
+
+function calcMomentUtilization() {
+    const ultMoment = ultMomentEntry.value;
+    const momentCapcity = momentCapcityEntry.value;
+    let util = ultMoment / momentCapcity;
+    momentUtilElement.innerHTML = util
+
 }
 
 calcButton.addEventListener("click", calculateMoment);
 
+function checkCapcity() {
+    calcMomentUtilization();
+}
