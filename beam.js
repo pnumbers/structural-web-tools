e4bdd54889c749a69c0e42b28a7ac769f306d555
pnumbers <pnumbers210@gmail.com>
2022-11-10 18:27:36 -0500

    Created new file for BeamWF class

--- beam.js
+++ beam.js
@@ -116,29 +116,3 @@ shapeSelector.addEventListener("change", shapeSelected)
 
 addShapesToDropDown()
 
-class BeamWF{
-    constructor(zx, fy){
-        this.zx = zx;
-        this.fy = fy;
-    }
-
-    calcPlasticMoment(){
-        this.plasticMoment = this.zx * this.fy;
-        return plasticMoment;
-    }
-
-    calcNominalMoment(){
-        this.calcPlasticMoment();
-        this.mn = Math.min(this.plasticMoment);
-        return this.mn;
-    }
-
-    calcFactoredMoment(){
-        // TODO: Verify phi factor of 0.9
-        // TODO: Add a way to change 0.9
-        this.phi = 0.9;
-        this.calcNominalMoment();
-        this.phiMn = this.phi * this.mn;
-        return phiMn;
-    }
-}
